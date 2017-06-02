6ac8cf9acf50072467f5315b82699a425ef69085
Gonzalo Rocha <genbx21@gmail.com>
2017-06-02 11:07:42 -0500

    A metadata.json file is created at server startup

--- readTracks.js
+++ readTracks.js
@@ -1,15 +1,35 @@
 var fs = require('fs');
-var p = 'public/tracks/';
+var nodeID3 = require('node-id3');
+var path = require('path');
+var tracksPath = './public/tracks/';
+var dataPath = './public/data/';
 
-fs.readdir(p,function(err,files){
+fs.readdir(tracksPath,function(err,files){
 	if(err) {
 		throw err;
 	}
-	fs.writeFile("./public/data/tracks.json",JSON.stringify(files), function(err){
+	//Read the tracks filenames
+	fs.writeFile(path.join(dataPath,"tracks.json"),JSON.stringify(files), function(err){
 		if (err){
 			throw err;
 		}
 		console.log("Tracks JSON created succesfully");
 	});
+	//Read the tracks metadata
+	var tracksMetadata = [];
+	files.forEach(function(trackName){
+		var trackFile = nodeID3.read(tracksPath + trackName);
+		var metadata = {
+			"filename" : trackName,
+			"title" : trackFile.title,
+			"artist" : trackFile.artist
+		};
+		tracksMetadata.push(metadata);
+	});
+	fs.writeFile(path.join(dataPath, 'metadata.json'), JSON.stringify(tracksMetadata), function(err){
+		if(err){
+			throw err;
+		}
+		console.log("Tracks Metadata JSON created succesfully");
+	});
 });
-	
