fd90082adbe56cff64750f18ad2489af7c6e9094
Gonzalo Rocha <genbx21@gmail.com>
2017-06-08 14:26:11 -0500

    Added code to call the port from the env var: close #13

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('Listening on port 3000');
 });
