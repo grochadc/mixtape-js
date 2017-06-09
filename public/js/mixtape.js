27182e72f976f50a93d36b6d37626dc5389694f7
Gonzalo Rocha <genbx21@gmail.com>
2017-06-08 20:14:33 -0500

    Include some debugging functions. Some variables are forced into the global scope

--- public/js/mixtape.js
+++ public/js/mixtape.js
@@ -1,18 +1,21 @@
 $(function() {
 
+
 	// Initialize the player, tracks array as well as track number and time
 	var player = $('#player');
-	window.tracks = []; //This array will be populated with the filenames from metadata.json
-	var currentTrack = Cookie.get('track',0);
-	var currentTrackTime = Cookie.get('time',0); 
+        window.tracks = []; //This array will be populated with the filenames from metadata.json
+	window.currentTrack = Cookie.get('track',0);
+	window.currentTrackTime = Cookie.get('time',0);
 
 	//jQuery AJAX to push the JSON from tracks.json to the tracks array defined earlier.
 	$.getJSON("../data/metadata.json", function(data){
+
+		console.log('Entered getJSON', data);
 		$.each(data, function(key, val){
+			console.log('Entered .each. Iterator',key,val);
 			tracks.push(val.filename);
 		});
 		setupPlayer(); //execute the rest of the code when the async $.getJSON is finished
-		console.log('player set up');
 	});
 
 	//Get the metadata for the playlist and append it to the #tracklist div
@@ -29,22 +32,31 @@ $(function() {
 	});
 
 	//Create a function that sets up the player source, time and events
-	function setupPlayer(){
-		var currentTrackSource = './tracks/' + tracks[currentTrack];
+	window.setupPlayer = function(){
+		window.currentTrackSource = './tracks/' + tracks[currentTrack];
 		player[0].src = currentTrackSource;
 		player[0].currentTime = currentTrackTime;
 
 		player.bind('pause', function(){
 			timeStopped = player[0].currentTime;
 			console.log('Player paused at ' + timeStopped + ' track ' + currentTrack);
+			console.log(document.cookie);
 			Cookie.set('track',currentTrack);
-			Cookie.set('time',timeStopped);
+			Cookie.set('time',currentTrackTime);
 		});
 
 		player.bind('ended', function(){
 			if(currentTrack<=tracks.length){
+				console.log('Still songs to play');
+
 				currentTrack++;
-				Cookie.set('track',currentTrack);
+
+				Cookie.set('track', currentTrack);
+
+				currentTrackSource = './tracks/' + tracks[currentTrack];
+
+				debugInfo();
+
 				player[0].src = currentTrackSource;
 				player[0].play();
 			}
@@ -52,7 +64,26 @@ $(function() {
 			else{
 				console.log('Reached the end of the mixtape');
 				Cookie.set('track',0);
+				Cookie.set('time',0);
 			}
+		console.log('player set up');
 		});
-	}
+	};
+
+	window.debugInfo = function(){
+		console.log('tracks: ' + tracks);
+		console.log('currentTrack: ' + currentTrack);
+		console.log('currentTrackTime: ' + currentTrackTime);
+		console.log('currentTrackSource: ' + currentTrackSource);
+		console.log('player: ' + JSON.stringify(player));
+		console.log('localStorage: ' + JSON.stringify(localStorage[0]));
+		console.log('tracks[currentTrack]: ' + tracks[currentTrack] );
+		console.log('tracks.length: ',tracks.length);
+		console.log('currentTrack <= tracks.length: ',Boolean(currentTrack <= tracks.length));
+	};
+
+	window.endTrack = function(){
+		//Code to force the current track to end on Console
+		player.trigger("ended");
+	};
 });
